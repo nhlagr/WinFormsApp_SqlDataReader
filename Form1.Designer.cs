e68763aec40e8b95f36c2711044907cb452904a1
Nihal Ağar <nihal.agar@advancity.com.tr>
2022-05-27 13:30:50 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WinFormsApp_SqlDataReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGetCategories = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCategoryId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.txtCategoriDescription = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(392, 356);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CurrentCellChanged += new System.EventHandler(this.dataGridView1_CurrentCellChanged);
+            // 
+            // btnGetCategories
+            // 
+            this.btnGetCategories.Location = new System.Drawing.Point(12, 12);
+            this.btnGetCategories.Name = "btnGetCategories";
+            this.btnGetCategories.Size = new System.Drawing.Size(147, 56);
+            this.btnGetCategories.TabIndex = 1;
+            this.btnGetCategories.Text = "Get Categories";
+            this.btnGetCategories.UseVisualStyleBackColor = true;
+            this.btnGetCategories.Click += new System.EventHandler(this.btnGetCategories_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCategoryId);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnRemove);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.btnAddNew);
+            this.groupBox1.Controls.Add(this.txtCategoriDescription);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtCategoryName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(431, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(264, 356);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "InsertUpdateDelete";
+            // 
+            // txtCategoryId
+            // 
+            this.txtCategoryId.Location = new System.Drawing.Point(0, 86);
+            this.txtCategoryId.MaxLength = 15;
+            this.txtCategoryId.Name = "txtCategoryId";
+            this.txtCategoryId.ReadOnly = true;
+            this.txtCategoryId.Size = new System.Drawing.Size(252, 23);
+            this.txtCategoryId.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Category Id";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(177, 327);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 6;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(87, 327);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(84, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(6, 327);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 4;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // txtCategoriDescription
+            // 
+            this.txtCategoriDescription.Location = new System.Drawing.Point(0, 223);
+            this.txtCategoriDescription.MaxLength = 1000;
+            this.txtCategoriDescription.Multiline = true;
+            this.txtCategoriDescription.Name = "txtCategoriDescription";
+            this.txtCategoriDescription.Size = new System.Drawing.Size(252, 84);
+            this.txtCategoriDescription.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Categori Description";
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.Location = new System.Drawing.Point(0, 151);
+            this.txtCategoryName.MaxLength = 15;
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(252, 23);
+            this.txtCategoryName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Categori Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 467);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnGetCategories);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGetCategories;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.TextBox txtCategoriDescription;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCategoryName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtCategoryId;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp_SqlDataReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connectionString = "Server=NIHALAGAR;Database=NORTHWND;Trusted_Connection=True;";
+        private void btnGetCategories_Click(object sender, EventArgs e)
+        {
+            LoadCategories();
+        }
+
+        private void LoadCategories()
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString;
+
+            string query = "select * from Categories";
+            SqlCommand command = new SqlCommand();
+            command.CommandText = query;
+            command.Connection = connection;
+
+            DataTable table = new DataTable();
+            table.Columns.Add("Id", typeof(int));
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Desc", typeof(string));
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();//commandda okuma amacıyla yukarıdakısorguyu calıştır dıyoruz. Okuma yaptıkca don ıcın while dongusu kuruyoruz.
+
+            while (reader.Read())
+            {
+                DataRow row = table.NewRow();
+                //row["CategoryId"] = (int)reader["CategoryId"];//okuma yontemı alttakı de farklı bır okuma yontemı
+                row["Id"] = reader.GetInt32("CategoryId");
+                row["Name"] = reader.GetString("CategoryName");
+                row["Desc"] = reader.GetString("Description");
+                table.Rows.Add(row);
+            }
+            //row yukarıda verilen kolon adlarına gore yazılmalı dbye gore değil ama readre dbye gore yazılmalı.
+
+
+            connection.Close();
+
+            command.Dispose();
+            connection.Dispose();//bılgısayarın ramınden bağlantı nesnesı sılınır yok edılır.
+
+
+            dataGridView1.DataSource = table;
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "insert into Categories (CategoryName,Description) VALUES (@CatagoryName,@Description)";
+            SqlCommand command = new SqlCommand(query,connection);
+
+            command.Parameters.AddWithValue("@CatagoryName", txtCategoryName.Text);
+            command.Parameters.AddWithValue("@Description", txtCategoriDescription.Text);
+
+            connection.Open();
+
+            int result = command.ExecuteNonQuery();
+            connection.Close();
+
+            if (result>0)
+            {
+                MessageBox.Show("Yeni kategori eklendi.");
+                LoadCategories();
+            }
+
+        }
+
+        private void dataGridView1_CurrentCellChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                txtCategoryId.Text = dataGridView1.CurrentRow.Cells["Id"].Value.ToString();
+                txtCategoryName.Text = dataGridView1.CurrentRow.Cells["Name"].Value.ToString();
+                txtCategoriDescription.Text = dataGridView1.CurrentRow.Cells["Desc"].Value.ToString();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                return;
+            }//secılen satır yoksa çık gıt.
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "UPDATE Categories SET CategoryName = @CategoryName, Description = @Description where CategoryID=@CategoryID";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            //command.Parameters.AddWithValue("@CatagoryId", int.Parse(txtCategoryId.Text));
+            //command.Parameters.AddWithValue("@CatagoryId", (int)dataGridView1.CurrentRow.Cells["ID"].Value);
+            int catId = (int)dataGridView1.CurrentRow.Cells["ID"].Value;
+            command.Parameters.AddWithValue("@CategoryId", catId);//bunlar farklı kullanımlar
+            command.Parameters.AddWithValue("@CategoryName", txtCategoryName.Text);
+            command.Parameters.AddWithValue("@Description", txtCategoriDescription.Text);
+
+            connection.Open();
+
+            int ruselt = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            if (ruselt > 0)
+            {
+                MessageBox.Show("Kategori güncellendi");
+                LoadCategories();
+            }
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                return;
+            }//secılen satır yoksa çık gıt.
+
+            string catName= dataGridView1.CurrentRow.Cells["Name"].Value.ToString();
+            DialogResult dialogResult = MessageBox.Show($"{catName} isimli kategoriyi silmek istediğinize emin misiniz?","Silme İşlemi",MessageBoxButtons.YesNoCancel);
+
+            if (dialogResult != DialogResult.Yes)
+            {
+                return;
+            }
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "DELETE FROM Categories where CategoryID=@CategoryID";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            //command.Parameters.AddWithValue("@CatagoryId", int.Parse(txtCategoryId.Text));
+            //command.Parameters.AddWithValue("@CatagoryId", (int)dataGridView1.CurrentRow.Cells["ID"].Value);
+            int catId = (int)dataGridView1.CurrentRow.Cells["ID"].Value;
+            command.Parameters.AddWithValue("@CategoryId", catId);//bunlar farklı kullanımlar
+
+            connection.Open();
+
+            int ruselt = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            if (ruselt > 0)
+            {
+                MessageBox.Show("Kategori silindi");
+                LoadCategories();
+            }
+        }
+    }
+}
